3d1ed78ed76d7e234bfe1e98ade9e1ab1ccb2d55
Runor Adjekpiyede <kampkellykeys@gmail.com>
2019-01-12 17:52:39 +0100

    ft(signup-user): signups a user to app

--- schema.py
+++ schema.py
@@ -1,9 +1,10 @@
 import graphene
 from car import Query as CarQuery, Mutation as CarMutation
+from user import Mutation as UserMutation
 
 class Query(CarQuery):
     pass
-class Mutation(CarMutation):
+class Mutation(CarMutation, UserMutation):
     pass
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
